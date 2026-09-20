4e5b024eeb13bb3fdecdc60546b3a38a585e9272
Pragya Ghosh <pragyarashmighosh@gmail.com>
2026-09-20 12:53:20 +0530

    feat: modified navbar for mobile screen

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,12 +6,12 @@ export default function Navbar({ activeSection }: { activeSection: string }) {
   ];
   
   return (
-    <nav className="fixed top-0 left-0 right-0 z-[100] mx-auto flex h-auto w-full max-w-[1200px] flex-wrap items-center justify-between border-b border-border bg-bg px-section-x py-[15px] min-h-[80px] md:h-[100px] md:py-0">
+    <nav className="fixed top-0 left-0 right-0 z-[100] mx-auto flex h-auto w-full max-w-[1200px] flex-col items-center justify-center border-b border-border bg-bg px-section-x py-[15px] min-h-[80px] md:h-[100px] md:flex-row md:justify-between md:py-0">
       <a href="#home" className="text-[2.2rem] font-normal text-accent-dark no-underline font-['Monoton']">
         Decode
       </a>
       
-      <ul className="m-0 mt-3 flex w-full list-none flex-wrap gap-4 p-0 md:mt-0 md:w-auto md:gap-8">
+      <ul className="m-0 mt-3 flex w-full list-none flex-row justify-center gap-6 p-0 md:mt-0 md:w-auto md:justify-end md:gap-8">
         {links.map((link) => (
           <li key={link.id}>
             <a 
